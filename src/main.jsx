8f63e88ec6f4a5cbc7a04518a8ceaf40f9598a4d
Nathaly <nathalynunezmedina@gmail.com>
2024-08-08 13:25:42 -0500

    "ADD sortBy and filters logic"

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./styles/index.css";
 import App from "./components/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
